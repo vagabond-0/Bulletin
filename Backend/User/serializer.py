a2728b758c5d39e913c0bd9ae02acca5535a2935
amalendu123 <220229@tkmce.ac.in>
2024-10-17 22:23:09 +0530

    done

--- Backend/User/serializer.py
+++ Backend/User/serializer.py
@@ -7,15 +7,16 @@ class AlumniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alumni
         fields = ['id', 'username', 'email', 'company', 'designation', 'profile_picture_url']
-
-class PostSerializer(serializers.ModelSerializer):
-    alumni_username = serializers.CharField(source='alumni.user.username', read_only=True)
-
-    class Meta:
-        model = Post
-        fields = ['id', 'alumni_username', 'posted_date', 'description', 'image_link', 'video_link', 'likes']
-        read_only_fields = ['posted_date', 'likes']
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'post', 'alumni', 'comment_text', 'posted_date')
+
+class PostSerializer(serializers.ModelSerializer):
+    alumni_username = serializers.CharField(source='alumni.user.username', read_only=True)
+    comments = CommentSerializer(many=True, read_only=True) 
+
+    class Meta:
+        model = Post
+        fields = ['id', 'alumni_username', 'posted_date', 'description', 'image_link', 'video_link', 'likes', 'comments'] 
+        read_only_fields = ['posted_date', 'likes']
